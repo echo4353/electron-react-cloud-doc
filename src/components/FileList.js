a0e8b485d4f495eddd8e2163112ff6379cc96ef2
pitersu <49382637+pitersu@users.noreply.github.com>
2022-05-03 17:20:55 +0800

    Add files via upload

--- src/components/FileList.js
+++ src/components/FileList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit,faTrash,faTimes } from "@fortawesome/free-solid-svg-icons";
 import { faMarkdown } from "@fortawesome/free-brands-svg-icons";
--- src/components/FileSearch.js
+++ src/components/FileSearch.js
@@ -2,31 +2,26 @@ import React, { useState, useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch,faTimes } from "@fortawesome/free-solid-svg-icons";
 import PropTypes from 'prop-types'
+import useKeyPress from "../hooks/useKeyPress";
 
 const FileSearch = ({ title,onFileSearch })=>{
   const [inputActive,setInputActive] = useState(false)
   const [value,setValue] = useState('')
-  
+  const enterPressed = useKeyPress(13)
+  const escPressed = useKeyPress(27)
   const node = useRef(null)
 
-  const closeSearch = (e)=>{
-    e.preventDefault()
+  const closeSearch = ()=>{
     setInputActive(false)
     setValue('')
   }
 
   useEffect(()=>{
-    const handleInputEvent = (event)=>{
-      const {keyCode} = event
-      if(keyCode === 13 && inputActive){
-        onFileSearch(value)
-      }else if(keyCode === 27 && inputActive){
-        closeSearch(event)
-      }
+    if(enterPressed && inputActive){
+      onFileSearch(value)
     }
-    document.addEventListener('keyup',handleInputEvent)
-    return ()=>{
-      document.removeEventListener('keyup',handleInputEvent)
+    if(escPressed && inputActive){
+      closeSearch()
     }
   })
 
@@ -37,7 +32,7 @@ const FileSearch = ({ title,onFileSearch })=>{
   },[inputActive])
 
   return (
-    <div className="alert alert-primary d-flex justify-content-between align-items-center">
+    <div className="alert alert-primary d-flex justify-content-between align-items-center mb-0">
       {
       !inputActive &&
       <>
