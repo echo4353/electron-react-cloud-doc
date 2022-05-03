0e8a2b9bc1ee2e0042f2a599a7ab34e720a3c9cb
pitersu <49382637+pitersu@users.noreply.github.com>
2022-05-03 15:03:57 +0800

    左侧面板开发完成

---
+++ src/src/utils/defaultFiles.js
@@ -0,0 +1,22 @@
+const defaultFiles = [
+  {
+    id:'1',
+    title:'first post',
+    body:'should be aware of this',
+    createdAt:123213123215
+  },
+  {
+    id:'2',
+    title:'second post',
+    body:'### be aware of title',
+    createdAt:1232131237654
+  },
+  {
+    id:'3',
+    title:'three post',
+    body:'are of this',
+    createdAt:4444413123215
+  },
+]
+
+export default defaultFiles
